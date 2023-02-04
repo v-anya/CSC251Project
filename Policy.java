40ad6fdfbfef5c865cbe8f25db450b524708ed8c
v-anya <anyamariapaula@hotmail.com>
2023-02-04 12:13:58 -0500

    Create Policy.java
    Added code for Policy class

---
+++ Policy.java
@@ -0,0 +1,207 @@
+public class Policy
+{
+   /**The policy class processes insurance information for one person*/
+   
+  private String policyNumber, //policy number
+                 provider,     //name of the provider
+                 firstName,    //policy holder's first name
+                 lastName,     // policy holder's last name
+                 smokingStatus; //policy holder's smoking status
+  private double age,           //policy holder's age
+                 height,        //policy holder's height in inches
+                 weight;        // policy holder's weight in pounds
+   
+  /**Constructor*/
+  
+  public Policy()
+  {
+      policyNumber = "0";
+      provider = "0";
+      firstName = "0";           // initializing all
+      lastName = "0";            //   the variables
+      smokingStatus = "0";
+      age = 0;
+      height = 0;
+      weight = 0;
+ 
+  }
+  
+  
+  /**Constructor
+  @param number the value sent to become the policy number
+  @param name the value sent to become the name of the provider
+  @param fname the value sent to become the policy holder's first name
+  @param lname the value sent to become the policy holder's last name
+  @param status the value sent to become the policy holder's smoking status
+  @param years the value sent to become the policy holder's age
+  @param inches the value sent to become the policy holder's height in inches
+  @param pounds the value sent to become the policy holder's weight in pounds*/
+  
+  public Policy(String number, String name, String fname, String lname, String status, double years, double inches, double pounds)
+  {
+  
+      policyNumber = number;
+      provider = name;
+      firstName = fname;           // assigning values to
+      lastName = lname;            //   the variables
+      smokingStatus = status;
+      age = years;
+      height = inches;
+      weight = pounds;
+      
+  }
+  
+  /**The setPolicyNumber method assigns a value to the policyNumber variable
+  @param number the value sent to become the policy number*/
+  
+  public void setPolicyNumber(String number)
+  {
+      policyNumber = number;
+  } 
+   
+  /**The setProvider method assigns a value to the provider variable
+  @param name the value sent to become the name of the provider*/
+  
+  public void setProvider(String name)
+  {
+     provider = name;
+  } 
+   
+  /**The setFirstName method assigns a value to the firstName variable
+  @param name the value sent to become the policy holder's first name*/
+  
+  public void setFirstName(String name)
+  {
+      firstName = name;
+  }
+     
+  /**The setLastName method assigns a value to the lastName variable
+  @param name the value sent to become the policy holder's last name*/
+  
+  public void setLastName(String name)
+  {
+      lastName = name;
+  }
+     
+  /**The setSmokingStatus method assigns a value to the smokingStatus variable
+  @param status the value sent to become the policy holder's smoking status*/
+  
+  public void setSmokingStatus(String status)
+  {
+      smokingStatus = status;
+  }
+  
+  /**The setAge method assigns a value to the age variable
+  @param years the value sent to become the policy holder's age*/
+  
+  public void setAge(double years)
+  {
+      age = years;
+  }
+  
+  /**The setHeight method assigns a value to the height variable
+  @param inches the value sent to become the policy holder's height in inches*/
+  
+  public void setHeight(double inches)
+  {
+      height = inches;
+  }
+  
+  /**The setWeight method assigns a value to the weight variable
+  @param pounds the value sent to become the policy holder's weight in pounds*/
+  
+  public void setWeight(double pounds)
+  {
+      weight = pounds;
+  }
+
+  /**The getPolicyNumber method returns the value of the policyNumber variable
+  @return the policy number*/
+  
+  public String getPolicyNumber()
+  {
+      return policyNumber;
+  } 
+   
+  /**The getProvider method returns the value of the provider variable
+  @return the name of the provider*/
+  
+  public String getProvider()
+  {
+     return provider;
+  } 
+   
+  /**The getFirstName method returns the value of the firstName variable
+  @return the policy holder's first name*/
+  
+  public String getFirstName()
+  {
+      return firstName;
+  }
+     
+  /**The getLastName method returns the value of the lastName variable
+  @return the policy holder's last name*/
+  
+  public String getLastName()
+  {
+      return lastName;
+  }
+     
+  /**The getSmokingStatus method returns the value of the smokingStatus variable
+  @return the policy holder's smoking status*/
+  
+  public String getSmokingStatus()
+  {
+      return smokingStatus;
+  }
+  
+  /**The getAge method returns the value of the age variable
+  @return the policy holder's age*/
+  
+  public double getAge()
+  {
+      return age;
+  }
+  
+  /**The getHeight method returns the value of the height variable
+  @return the policy holder's height in inches*/
+  
+  public double getHeight()
+  {
+      return height;
+  }
+  
+  /**The getWeight method returns the value of the weight variable
+  @return the policy holder's weight in pounds*/
+  
+  public double getWeight()
+  {
+      return weight;
+  }
+  
+  /**The getBmi method calculates and returns the policy holder's BMI
+  @return the policy holder's BMI*/
+  
+  public double getBmi()
+  {
+      return (weight * 703 ) / (height * height) //BMI formula
+  }
+  
+  /**The getPrice method calculates the insurance price based the policy holder's information
+  @return the insurance price*/
+  
+  public double getPrice()
+  {
+      double price = 600; //base price for insurance
+      
+      if (age > 50) // adding fee for old age
+         price += 75;
+      
+      if (smokingStatus.equals("smoker") //adding fee for smoking
+         price += 100;
+      
+      if (getBmi() > 35) //adding fee for high BMI
+         price += ((getBmi() - 35) * 20) //formula to calculate extra fee
+         
+      return price;
+  }
